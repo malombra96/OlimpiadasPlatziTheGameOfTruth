b5e6e68e6f76ae2b089b505c2a7e4bf222dd929e
Brandon Malagon <malombrabmb@gmail.com>
2021-02-27 12:05:26 -0500

    arregle el movimiento del personaje.

--- Assets/Scripts/PlayerControle.cs
+++ Assets/Scripts/PlayerControle.cs
@@ -1,35 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
 
 public class PlayerControle : MonoBehaviour
-
-// Variables del movimiento del personaje
-public float jumpForce = 6f;
-Rigibody2D rigidBody;
-
-private void Awake()
 {
-    rigidBody = GetComponet<Rigibody2D>();
-}
-{
+    
+    // Variables del movimiento del personaje
+    public float jumpForce = 6f;
+    public Rigidbody2D rigidBody;
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetJey(Keycode.Space) || Input.GetMouseButtonDown(0) )
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0) )
             Jump();
-        {
-            
-        }
+        
+        print(Input.GetAxis("Horizontal"));
+
     }
 
     void Jump(){
         rigidBody.AddForce(Vector2.up* jumpForce, ForceMode2D.Impulse);
     }
 }
+
+
+
