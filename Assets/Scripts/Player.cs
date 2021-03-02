991796a61f32cbee6ff4e30d884917cc6761fe9d
unknown <you@example.comdc162005@gmail.com>
2021-03-02 18:13:34 -0500

    Aqui esta mi progreso

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,44 +4,58 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-  public class PlayerControle : MonoBehaviour
-{
-   private Rigidbody2D rb;
-   private Animator animator;
-   //
-   public LayerMask maskFloor;
-   public Transform testFloor;
-   public float ForceJump = 500f;
-   private bool IsWalking;
-   private bool IsFool = true;
-   private float radio = 0.07f;
-
+ private Rigidbody2D rb;
+ private Animator animator;
+ //
+ public float speed = 2f;
+public float maxSpeed = 10f; 
+  public LayerMask maskFloor;
+ public Transform testFloor;
+ public float forceJump = 500f;
+ private bool IsWalking;
+ private bool IsFloor;
+ private float radio = 0.07f;
+ private bool jump2 = false;
 
     // Start is called before the first frame update
     void Start(){
-       rb = GetComponent<Rigidbody2D>();
-       animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
-
-   void FixedUpdate(){
-
-      IsFoor = Physics2D.OverlapCircle(){
-
-   }
-
+     
     // Update is called once per frame
     void Update(){
 
-       if(Input.GetKeyDown(KeyCode.Space)){
-          rb.AddForce(new Vector2(0, ForceJump));
+        if(Input.GetKeyDown(KeyCode.Space)){
+
+            if(IsFloor || !jump2){
+                rb.velocity = new Vector2(rb.velocity.x, forceJump);
+            rb.AddForce(new Vector2(0, forceJump));
+            
+            if(!jump2 && !IsFloor){
+                jump2 = true;
+            }
+            }
        }
+
        if(Input.GetKeyDown(KeyCode.D)){
           animator.SetBool("IsWalking", true);
        }
+       
       if(Input.GetKeyUp(KeyCode.D)){
          animator.SetBool("IsWalking", false);
       }
+       
     }
-      
-        
-}
+    void FixedUpdate(){   
+        float h = Input.GetAxis("Horizontal");
+        rb.AddForce(Vector2.right * speed * h);
+
+        if(rb.velocity.x > maxSpeed){
+            rb.velocity = new Vector2(maxSpeed, rb.velocity.y);
+            
+        }
+        }
+    }    
+   
+ 
